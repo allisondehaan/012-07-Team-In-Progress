36d3eea0420aa7edd82ea2764e7d628a8ed25598
Allison DeHaan <alde6209@colorado.edu>
2024-04-03 14:58:00 -0600

    server.spec.js

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,46 @@
+-- -----------------------------------------------------
+-- Table Prefrences
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS Preferences CASCADE;
+CREATE TABLE Preferences(
+  idPref SERIAL PRIMARY KEY ,
+  sortingPref INT NULL,
+);
+
+-- -----------------------------------------------------
+-- Table User
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS Users CASCADE;
+CREATE TABLE Users (
+  idUser SERIAL PRIMARY KEY,
+  email VARCHAR(45) NOT NULL,
+  userName VARCHAR(45) NOT NULL,
+  passWordHash VARCHAR(45) NOT NULL,
+  firstName VARCHAR(45) NOT NULL,
+  lastName VARCHAR(45) NULL,
+  Usercol VARCHAR(45) NULL,
+  idPref INT NOT NULL,
+);
+
+
+-- -----------------------------------------------------
+-- Table TODO
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS TODO CASCADE;
+CREATE TABLE TODO (
+  idTODO SERIAL PRIMARY KEY,
+  eventDate DATE NOT NULL,
+  eventTime TIME NOT NULL,
+  eventTitle VARCHAR(45) NOT NULL,
+  eventDesc VARCHAR(4500) NULL,
+  eventLocation VARCHAR(70) NULL,
+  );
+
+-- -----------------------------------------------------
+-- TableUser_to_TODOS
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS Users_toTODO CASCADE;
+CREATE TABLE Users_to_TODO (
+  idTODO INT NOT NULL,
+  idUser INT NOT NULL,
+);
--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -30,3 +30,47 @@ describe('Server!', () => {
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
 // ********************************************************************************
+
+//We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 400 along with a "Invalid input" message.
+
+  // Example Negative Testcase :
+  // API: /add_user
+  // Input: {id: 5, name: 10, dob: '2020-02-20'}
+  // Expect: res.status == 400 and res.body.message == 'Invalid input'
+  // Result: This test case should pass and return a status 400 along with a "Invalid input" message.
+  // Explanation: The testcase will call the /add_user API with the following invalid inputs
+  // and expects the API to return a status of 400 along with the "Invalid input" message.
+
+  describe('Testing Add User API', () => {
+    it('positive : /add_user', done => {
+      // Refer above for the positive testcase implementation
+      chai
+      .request(server)
+      .post('/add_user')
+      .send({idUser: 5, email:'johndoe@colorado.edu',userName: 'John Doe', firstName: 'John', lastName: 'Doe'})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Success');
+        done();
+      });
+    });
+  
+    // Example Negative Testcase :
+    // API: /add_user
+    // Input: {id: 5, name: 10, dob: '2020-02-20'}
+    // Expect: res.status == 400 and res.body.message == 'Invalid input'
+    // Result: This test case should pass and return a status 400 along with a "Invalid input" message.
+    // Explanation: The testcase will call the /add_user API with the following invalid inputs
+    // and expects the API to return a status of 400 along with the "Invalid input" message.
+    it('Negative : /add_user. Checking invalid name', done => {
+      chai
+        .request(server)
+        .post('/add_user')
+        .send({idUser: 5, email:'johndoe@colorado.edu',userName: 'John Doe', firstName: 'John', lastName: 'Doe'})
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.message).to.equals('Invalid input');
+          done();
+        });
+    });
+  });
